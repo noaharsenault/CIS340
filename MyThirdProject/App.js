5e4e47b968613642aad72dc9cb65b112efcd749c
noaharsenault <103395975+noaharsenault@users.noreply.github.com>
2022-04-13 22:20:04 +0000

    Created Third Project Video 2

---
+++ MyThirdProject/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Text, View, Button } from 'react-native';
+
+function TV(props) {
+
+  const [isOff, setIsOff] = useState(true);
+
+  return (
+    <View>
+      <Text>
+        {"\n\n\n\n\n"}
+        This is {props.name} TV, and it is {isOff ? "Off" : "Turned On"}!
+      </Text>
+      <Button
+      onPress={() => {
+        setIsOff(false);
+      }}
+      disabled={!isOff}
+      title={isOff ? "Turn Me On, Please!" : "Thank You!"}
+      />
+    </View>
+  );
+}
+
+export default function MultiTVs(){
+  return(
+    <View>
+      <TV name="LG" />
+      <TV name="Sony" />
+    </View>
+  );
+}
