9767f7410a908724891293cd67bb927eb6988a4b
noaharsenault <103395975+noaharsenault@users.noreply.github.com>
2022-04-22 03:38:17 +0000

    My Fourth Project

---
+++ MyFourthProject/App.js
@@ -0,0 +1,28 @@
+import React from 'ereact';
+import { Text, View, SectionList } from 'react-native';
+
+export default StatesApp = () => {
+  return (
+    <View style={{flex: 1, paddingTop:22}}>
+      <SectionList
+      sections={[
+      {title: 'A', data: ['Alabama', 'Alaska', 'Arizona', 'Arkansas']},
+      {title: 'C', data: ['California', 'Colorado', 'Connecticut']},
+      {title: 'D', data: ['Delaware']},
+      {title: 'F', data: ['Florida']},
+      {title: 'G', data: ['Georgia']},
+      {title: 'H', data: ['Hawaii']},
+
+      ]}
+      renderItem={({item}) => <Text style={{padding: 10, fontSize: 20, height: 44,}}> {item} </Text>}
+      renderSectionHeader={({section}) => <Text style={{paddingTop: 4, paddingLeft: 10,
+      paddingRight: 10,
+      paddingBottom: 4,
+      fontSize: 14,
+      fontWeight: 'bold',
+      backgroundColor: '#9FA8DF',}}>{section.title}</Text>}
+      keyExtractor={(item,index) =>index}
+    />
+    </View>
+  );
+}
